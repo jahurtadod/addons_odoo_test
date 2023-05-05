5ab2e1e93568f99c2d2b31af0fd255c7f684de0b
Jorge <jahurtadod@outlook.com>
2023-05-05 17:29:30 -0500

    add exercise three

--- 01_add_hide_delete/models/ej_pet.py
+++ 01_add_hide_delete/models/ej_pet.py
@@ -6,3 +6,12 @@ class EjPet(models.Model):
     _inherit = 'ej.pet'
     piel = fields.Char(string='Piel', default='Verde')
     paseo = fields.Boolean(string='Paseo')
+
+    is_racism = fields.Boolean(string='Is racism', compute='_compute_is_racism')
+
+    date_b = fields.Date('Fecha', default=fields.Date.today)
+
+    @api.depends('piel')
+    def _compute_is_racism(self):
+        for record in self:
+            record.is_racism = record.piel == 'Negro'
